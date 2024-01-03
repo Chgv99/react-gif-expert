288f8a126f47bdf7720cf35c2617493126001bc6
Christian Garcia Viguera <christiangv99@gmail.com>
2024-01-03 09:24:17 +0000

    Testing

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 
 export const GifItem = ( { title, url, id } ) => {
     return (
@@ -7,3 +8,18 @@ export const GifItem = ( { title, url, id } ) => {
         </div>
     )
 }
+
+GifItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+}
+
+/* Tarea
+
+1. Añadir proptypes ?????
+    a. title obligatorio
+    b. url obligatorio
+
+2. Evaluar el snapshot
+
+*/
---
+++ tests/demo.test.js
@@ -0,0 +1,6 @@
+
+describe('first', () => { 
+    test('should first', () => { 
+        expect(1).toBe(1);
+     })
+ })
